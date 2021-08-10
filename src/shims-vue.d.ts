c5ff643d67daad04824ba9a8dff19563506c5f16
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-10 14:02:08 +0800

    chore: update dts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,8 +1,3 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
-/// <reference types="vite-plugin-vue-layouts/client" />
-/// <reference types="@intlify/vite-plugin-vue-i18n/client" />
-
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
