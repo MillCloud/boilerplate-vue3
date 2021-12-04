584e7ecfb1f86b2db737b476279d233ce3ee51d1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-04 20:50:45 +0800

    fix: fix App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view />
+  <el-config-provider>
+    <router-view />
+    <vue-query-dev-tools />
+  </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import { useQueryProvider } from 'vue-query';
+import { VueQueryDevTools } from 'vue-query/devtools';
+import { queryClient } from '@/utils';
+
+useQueryProvider(queryClient);
+</script>
