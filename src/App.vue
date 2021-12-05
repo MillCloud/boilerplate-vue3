5607dd7cea31e525edc8c6d6ba494e84125677e2
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-05 18:13:52 +0800

    fix: 修复默认语言为中文

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-config-provider>
+  <el-config-provider :locale="zhCn">
     <router-view />
     <vue-query-dev-tools />
   </el-config-provider>
@@ -8,6 +8,7 @@
 <script setup lang="ts">
 import { useQueryProvider } from 'vue-query';
 import { VueQueryDevTools } from 'vue-query/devtools';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import { queryClient } from '@/utils';
 
 useQueryProvider(queryClient);
