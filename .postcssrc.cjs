1e9eaf1092cb62a12b0ef21f4cce3a6bac88df57
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-20 13:15:39 +0800

    chore: update postcss config

--- .postcssrc.cjs
+++ .postcssrc.cjs
@@ -2,8 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     'postcss-preset-env': {
-      browsers:
-        'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0',
       stage: 3,
     },
   },
