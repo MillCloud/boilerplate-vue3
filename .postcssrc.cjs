361c2fb4084d7a377f29edf3cb89cede63c3c30c
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-20 11:45:42 +0800

    fix: fix config

--- .postcssrc.cjs
+++ .postcssrc.cjs
@@ -2,9 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
+      browsers:
+        'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0',
       stage: 3,
     },
   },
