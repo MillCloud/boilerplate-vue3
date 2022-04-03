109c416c94a4753e820f77102c0f9ec8d3b569d7
ModyQyW <wurui-dev@formail.com>
2022-04-03 18:42:55 +0800

    chore: remove vue-query-dev-tools

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <el-config-provider :locale="zhCn">
     <router-view />
-    <vue-query-dev-tools />
   </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
-import { VueQueryDevTools } from 'vue-query/devtools';
 </script>
